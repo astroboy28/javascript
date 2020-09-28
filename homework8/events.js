dbbf645db35bf3afbdaff702473af8524286c6b2
Del Icalla <40507255+astroboy28@users.noreply.github.com>
2020-09-28 13:03:51 +0900

    commit in master

---
+++ homework8/events.js
@@ -0,0 +1,5 @@
+let canAddCross = true;
+
+const topLeft = document.getElementById("top-left").querySelector("span");
+const topMiddle = document.getElementById("top-middle").querySelector("span");
+const topRight = document.getElementById("top-right").querySelector("span");
